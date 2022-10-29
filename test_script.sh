f41654fb62becffdd3b58ee9e9a5daf5cf1ab731
Arne <arne.carla.jacobs@outlook.com>
2022-10-30 00:52:27 +0200

    updated script to work as I want

--- test_script.sh
+++ test_script.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
+SESSION_NAME="CLIENT_SERVER_TEST"
+if test -z "$TMUX";
+then
+  # if not currently in a tmux session, create one create new windows
+  tmux kill-session -t $SESSION_NAME
+  tmux new-session -s $SESSION_NAME -d 'cargo run listen' \; split-window -d -h "cargo run speak; zsh" \; attach
+else
+  # if in a tmux session
+  LAST=$(tmux display-message -p '#I')
+  if tmux select-window -t $SESSION_NAME
+  then
+    # if a widow is already created, re-run commands
+    tmux select-pane -t 1 \; respawn-pane -k \; select-pane -t 2 \; respawn-pane -k
+    tmux select-window -t "$LAST"
+  else
+    # if a window is not created, create setup and run commands
+    tmux set-option -gt $SESSION_NAME remain-on-exit on
+    tmux new-window -n $SESSION_NAME "cargo run listen" \; split-window -t $SESSION_NAME -h "cargo run spreak" \; previous-window
+  fi
 
-tmux kill-session
-tmux new-session -d 'cargo run listen'
-tmux split-window -h 'cargo run speak; zsh'
-tmux -2 attach-session -d
+fi
